8246111ece91bc1cffa7a70891b96da4a0cbb784
Mudit_022 <muditjain2204@gmail.com>
2026-09-10 10:52:54 +0530

    Time: 1 ms (53.88%), Space: 9.6 MB (75.19%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -5,9 +5,22 @@ public:
             return false;
         }
 
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
+        // sort(s.begin(), s.end());
+        // sort(t.begin(), t.end());
 
-        return s == t;
+        // return s == t;
+        int count[26] = {0};
+        for(int i=0; i<s.length(); i++){
+            count[s[i] -'a']++;
+            count[t[i] - 'a']--;
+        }
+
+        for(int i=0; i<26; i++){
+            if(count[i] != 0)
+            return false;
+        }
+
+        return true;
+        
     }
 };
