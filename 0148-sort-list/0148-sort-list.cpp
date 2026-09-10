505db2ec307281cc6d36b3f79f46b0c8a52f2c92
Mudit_022 <muditjain2204@gmail.com>
2026-09-10 10:46:18 +0530

    Time: 11 ms (81.28%), Space: 57.1 MB (83.58%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -1,57 +1,44 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* sortList(ListNode* head) {
-        //base case0 or 1 node is already
-        if(!head || !head->next){
-            return head;
-        }
+        if (!head || !head->next) return head;
 
-        //pointre bhagane ka code
-        ListNode* prev = nullptr;
+        // Initialize fast to head->next so slow stops at the end of the first half
         ListNode* slow = head;
-        ListNode* fast = head;
+        ListNode* fast = head->next;
 
-        while(fast && fast->next){
-           prev = slow;
-           slow = slow->next;
-           fast = fast->next->next;
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
         }
-        prev -> next = nullptr;
+
+        // Split the list
+        ListNode* mid = slow->next;
+        slow->next = nullptr;
 
         ListNode* left = sortList(head);
-        ListNode* right  = sortList(slow);
+        ListNode* right = sortList(mid);
 
         return merge(left, right);
     }
 
-    private:
-        ListNode* merge(ListNode* l1 , ListNode* l2){
-            ListNode dummy(0);
-            ListNode* tail = &dummy;
+private:
+    ListNode* merge(ListNode* l1, ListNode* l2) {
+        ListNode dummy(0);
+        ListNode* tail = &dummy;
 
-            while(l1  && l2){
-                if(l1->val <= l2->val){
-                    tail->next = l1;
-                    l1 = l1->next;
-                }
-                else{
-                    tail->next = l2;
-                    l2 = l2->next;
-                }
-                tail = tail ->next;
+        while (l1 && l2) {
+            if (l1->val <= l2->val) {
+                tail->next = l1;
+                l1 = l1->next;
+            } else {
+                tail->next = l2;
+                l2 = l2->next;
             }
-
-            tail->next = l1 ? l1:l2;
-            return dummy.next;
+            tail = tail->next;
         }
+
+        tail->next = l1 ? l1 : l2;
+        return dummy.next;
+    }
 };
